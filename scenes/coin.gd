6ec59d4d0e5e5d676a456b7b4bcc1079c81d022c
AK3 <mjlabishek123@gmail.com>
2026-04-07 09:52:15 +0545

    added a pickup sound on picking a coin

--- scenes/coin.gd
+++ scenes/coin.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _on_body_entered(_body: Node2D) -> void:
 	game_manager.add_point()
-	queue_free()
+	animation_player.play("pickup")
+	
 	
 
 	
