93e83806a7a8fe670eb858be0d8efa71225f1071
AK3 <mjlabishek123@gmail.com>
2026-04-07 22:43:20 +0545

    i will remove the lvl and expand the entire world like geometry dash its al meesed up i have to clear everthing

--- script/level_select.gd
+++ script/level_select.gd
@@ -1,24 +1,6 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
-
-
 
 func _on_lvl_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
-	
-	#_on_lvl_2_pressed() -> void:
-	get_tree().change_scene_to_file()
-	
-	#_on_lvl_3_pressed() -> void:
-	get_tree().change_scene_to_file()
-	
--- script/player.gd
+++ script/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 130.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sfx_jump: AudioStreamPlayer2D = $sfx_jump
+@onready var sfx_run: AudioStreamPlayer2D = $sfx_run
 
 
 func _physics_process(delta: float) -> void:
@@ -14,6 +16,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		sfx_jump.play()
 
 	# direction
 	var direction := Input.get_axis("left", "right")
@@ -39,7 +42,10 @@ func _physics_process(delta: float) -> void:
 	# apply movement
 	if direction:
 		velocity.x = direction * SPEED
+		
 	else:
+		
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 
 	move_and_slide()
