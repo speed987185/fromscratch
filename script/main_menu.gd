5e6a14370ed9be2580fe14d2f4fb258f111664f4
AK3 <mjlabishek123@gmail.com>
2026-04-07 13:04:10 +0545

    added a menu to a game hop and quit work now i will make a section for a levels and also make few levels

---
+++ script/main_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_hop_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	
+
+
+func _on_levels_pressed() -> void:
+	print("levels")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
